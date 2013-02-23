e605cd6edf8f24c776f6c2005578467741cd80c8
Falko Schmidt <scf@panter.ch>
2013-02-23 12:12:05 +0100

    refactor reporting logic, add missing album art detection.

--- report.rb
+++ report.rb
@@ -1,26 +1,57 @@
 # MP3 consistency reporter
 #
-# Finds out if there's something wrong with albums in your
-# collection.
-# It should
-#  * find tracks without album art
-#  * find tracks with more than one album art
-#  * find tracks with id3v1 tags
-#  * find tracks with < id3v2.4 tags
-#  * find tracks with album artist set
-#  * find tracks that have a text format other than UTF-8
-#  * group by artist and find differing genres
-#  * group results by album
-#
-# TODO
-# 1. Support all audio formats (ogg, mp4, aac, flac, ...)
-# 2. Ignore albums that
-#    - have no id3v1 tag in any of their tracks
-#    - have a picture in all their tracks
+# Author: Falko Schmidt <kaethorn@gmail.com>
 
 require 'taglib'
 require 'find'
 require 'mimemagic'
+require 'pp'
+
+def report_id3v1_tag(store, file_tag, file)
+  id3v1_tag = file_tag.id3v1_tag
+  unless id3v1_tag.empty?
+    store[id3v1_tag.artist] ||= {}
+    store[id3v1_tag.artist][id3v1_tag.album] = File.dirname file
+  end
+end
+
+def report_missing_art(store, file_tag, file)
+  id3v2_tag = file_tag.id3v2_tag
+  if id3v2_tag.frame_list('APIC').length < 1
+    store[id3v2_tag.artist] ||= {}
+    store[id3v2_tag.artist][id3v2_tag.album] = File.dirname file
+  end
+end
+
+def main(directory)
+  id3v1_albums = {}
+  missing_art_albums = {}
+
+  Find.find(directory) do |file|
+    # Only consider files
+    unless FileTest.directory?(file)
+      raw_file = File.open file
+      fileType = MimeMagic.by_path(file)
+      magicType = MimeMagic.by_magic(raw_file)
+      raw_file.close
+
+      # Only consider audio files
+      if fileType == 'audio/mpeg' and magicType == 'audio/mpeg'
+        TagLib::MPEG::File.open(file) do |file_tag|
+
+          # Find tracks with id3v1 tags
+          report_id3v1_tag id3v1_albums, file_tag, file
+
+          # Find tracks with missing album art
+          report_missing_art missing_art_albums, file_tag, file
+        end
+      end
+    end
+  end
+
+  pp id3v1_albums
+  pp missing_art_albums
+end
 
 dir = ARGV[0]
 unless dir
@@ -28,20 +59,4 @@ unless dir
   exit
 end
 
-Find.find(dir) do |file|
-  unless FileTest.directory?(file)
-    raw_file = File.open file
-    fileType = MimeMagic.by_path(file)
-    magicType = MimeMagic.by_magic(raw_file)
-    raw_file.close
-
-    if fileType == 'audio/mpeg' and magicType == 'audio/mpeg'
-      TagLib::MPEG::File.open(file) do |f|
-        id3v1_tag = f.id3v1_tag
-        id3v2_tag = f.id3v2_tag
-        puts "#{id3v1_tag.artist}: #{file}" if id3v1_tag
-        #puts "id3v2: #{file}" if id3v2_tag
-      end
-    end
-  end
-end
+main dir
