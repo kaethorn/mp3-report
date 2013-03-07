2a6b2f130f3609b2a29f78e0e13233b395c0d9d5
Falko Schmidt <scf@panter.ch>
2013-03-07 13:59:23 +0100

    implement missing mp3 reporters

--- lib/scanner_mp3.cxx
+++ lib/scanner_mp3.cxx
@@ -3,6 +3,7 @@
 #include <id3v2tag.h>
 #include <id3v1tag.h>
 #include <id3v2frame.h>
+#include <id3v2header.h>
 
 void MP3Scanner::scan() {
   TagLib::MPEG::File file_tag(file.c_str());
@@ -28,20 +29,20 @@ void MP3Scanner::report_id3v1_tag(TagLib::MPEG::File *file_tag) {
   }
 }
 void MP3Scanner::report_missing_art(TagLib::MPEG::File *file_tag) {
-  // id3v2_tag = file_tag.id3v2_tag
-  // if id3v2_tag.frame_list('APIC').length < 1 
-  //   add_to_report id3v2_tag.artist, id3v2_tag.album, File.dirname(@file), :missing_art
-  // end 
+  TagLib::ID3v2::Tag *id3v2_tag = file_tag->ID3v2Tag();
+  if (id3v2_tag->frameListMap()["APIC"].isEmpty()) {
+    add_to_report(id3v2_tag->artist().to8Bit(true), id3v2_tag->album().to8Bit(true), dirname(file), "missing_art");
+  }
 }
 void MP3Scanner::report_multiple_art(TagLib::MPEG::File *file_tag) {
-  // id3v2_tag = file_tag.id3v2_tag
-  // if id3v2_tag.frame_list('APIC').length > 1 
-  //   add_to_report id3v2_tag.artist, id3v2_tag.album, File.dirname(@file), :multiple_art
-  // end 
+  TagLib::ID3v2::Tag *id3v2_tag = file_tag->ID3v2Tag();
+  if (id3v2_tag->frameListMap()["APIC"].size() > 1) {
+    add_to_report(id3v2_tag->artist().to8Bit(true), id3v2_tag->album().to8Bit(true), dirname(file), "multiple_art");
+  }
 }
 void MP3Scanner::report_obsolete_id3v2_version(TagLib::MPEG::File *file_tag) {
-  // id3v2_tag = file_tag.id3v2_tag
-  // if id3v2_tag.header.major_version < 4 
-  //   add_to_report id3v2_tag.artist, id3v2_tag.album, File.dirname(@file), :obsolete_id3v2_version
-  // end
+  TagLib::ID3v2::Tag *id3v2_tag = file_tag->ID3v2Tag();
+  if (id3v2_tag->header()->majorVersion() < 4) {
+    add_to_report(id3v2_tag->artist().to8Bit(true), id3v2_tag->album().to8Bit(true), dirname(file), "obsolete_id3v2_version");
+  }
 }
