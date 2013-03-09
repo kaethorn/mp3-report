ebcfbc9beff28efe6c009e098bf3ae82cc5cfd27
Falko Schmidt <scf@panter.ch>
2013-03-10 00:33:48 +0100

    notify about missing audio file support.

--- src/reporter/reporter.cxx
+++ src/reporter/reporter.cxx
@@ -150,6 +150,8 @@ void Reporter::iterate_directory() {
     } else if (file_type == "audio/mp4") {
       MP4Scanner mp4_scanner(file->path().string(),&report);
       mp4_scanner.scan();
+    } else if (file_type.find("audio/") != string::npos) {
+      cout << "Missing support for type " << file_type << endl;
     }
   }
 }
